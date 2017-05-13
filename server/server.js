68b2409a97dd79ead2177d0fbb28663c68337bc6
Brett East <brett@sproutback.com>
2017-05-13 15:17:08 +1000

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -99,6 +99,19 @@ app.patch('/todos/:id', (req, res) => {
   })
 });
 
+  // POST /users - stop server, wipe database, start server
+  app.post('/users', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+    var user = new User(body);
+
+    user.save().then(() => {
+      return user.generateAuthToken();
+    }).then((token) => {
+      res.header('x-auth', token).send(user);
+    }).catch((err) => {
+      res.status(400).send(err);
+    });
+  });
 
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`);
